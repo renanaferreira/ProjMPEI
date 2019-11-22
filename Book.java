a76b3339d5a82ab1ac93a63ffc435f6bb331c2bc
Rtonne <58034455+Rtonne@users.noreply.github.com>
2019-11-22 18:00:43 +0000

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,46 @@
+package library;
+
+public class Book {
+	private String title;
+	private String author;
+	private boolean available;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public boolean isAvailable() {
+		return available;
+	}
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+	public int hashCode(int k) {
+		if (k > 0) {
+			final int prime = 31;
+			int result = 1;
+			for (int i = 1; i <= k; i++) {
+				result = i * prime * result + ((author == null) ? 0 : author.hashCode());
+				result = i * prime * result + ((title == null) ? 0 : title.hashCode());
+			}
+			return result;
+		} else {
+			return this.hashCode();
+		}
+	}
+}
