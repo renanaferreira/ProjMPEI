c6977fcf9b9df9c3403a6d29a1d628fa97870cb8
Rtonne <58034455+Rtonne@users.noreply.github.com>
2019-11-22 18:01:40 +0000

    Add files via upload

--- MPEI_Library/src/library/Book.java
+++ MPEI_Library/src/library/Book.java
@@ -22,4 +22,25 @@ public class Book {
 	public void setAvailable(boolean available) {
 		this.available = available;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+	public int hashCode(int k) {
+		if (k > 0) {
+			final int prime = 31;
+			int result = 1;
+			for (int i = 1; i <= k; i++) {
+				result = i * prime * result + ((author == null) ? 0 : author.hashCode());
+				result = i * prime * result + ((title == null) ? 0 : title.hashCode());
+			}
+			return result;
+		} else {
+			return this.hashCode();
+		}
+	}
 }
