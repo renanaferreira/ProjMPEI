e463b2723ee3ae20988188689542f1d8ca791798
Rtonne <58034455+Rtonne@users.noreply.github.com>
2019-11-28 10:39:48 +0000

    Update LibraryMain.java

--- MPEI_Library/src/library/LibraryMain.java
+++ MPEI_Library/src/library/LibraryMain.java
@@ -6,7 +6,7 @@ public class LibraryMain {
 		Book b1 = new Book("Python","Pedro");
 		Book b2 = new Book("Java","Renan");
 		Book b3 = new Book("C++","Pedro");
-		BloomFilter books = new BloomFilter(500);
+		BloomFilter books = new BloomFilter(500,3);
 		books.insert(b1);
 		books.insert(b2);
 		System.out.println(books.contains(b1));
