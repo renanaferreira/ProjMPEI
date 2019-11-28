19339b2893d26754426c53d4469f459997104f0c
Rtonne <plcasimiro2000@gmail.com>
2019-11-28 10:32:52 +0000

    Added an argument to BloomFilter

--- MPEI_Library/src/library/BloomFilter.java
+++ MPEI_Library/src/library/BloomFilter.java
@@ -8,20 +8,22 @@ public class BloomFilter {
 	//private List<Boolean> array = new ArrayList<>();
 	private boolean[] array;
 	private int max_size;
-	public BloomFilter(int size) {
+	private int nfunc;
+	public BloomFilter(int size, int k) {
 		max_size = size;
 		//array = Arrays.asList(new Boolean[size]);
 		array = new boolean[size];
+		nfunc = k;
 	}
 	public void insert(Book book) {
-		for (int k = 1; k < 4; k++) {
+		for (int k = 1; k <= nfunc; k++) {
 			int hash = (book.hashCode(k) & 0x7fffffff);
 			int index = hash%max_size;
 			array[index] = true;
 		}
 	}
 	public boolean contains(Book book) {
-		for (int k = 1; k < 4; k++) {
+		for (int k = 1; k <= nfunc; k++) {
 			int hash = (book.hashCode(k) & 0x7fffffff);
 			int index = hash%max_size;
 			if (!array[index]) {
